e1df56d21634a77915bdb628b19c6c496a48ad96
Ben-Huang <824451333@qq.com>
2024-10-18 09:31:27 +0800

    Update diy-part2-gateway.sh
    修改ip ,改用passwall2

--- diy-part2-gateway.sh
+++ diy-part2-gateway.sh
@@ -58,7 +58,7 @@ function clean_packages(){
 sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' package/base-files/files/etc/shadow
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.1.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.5/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
@@ -154,15 +154,15 @@ mv package/custom/golang feeds/packages/lang/
 # argon 主题
 config_package_add luci-theme-argon
 ## passwall
-config_package_add luci-app-passwall
-config_package_add luci-app-passwall_Nftables_Transparent_Proxy
-config_package_del luci-app-passwall_Iptables_Transparent_Proxy
-config_package_del luci-app-passwall_INCLUDE_Shadowsocks_Libev_Client
-config_package_del luci-app-passwall_INCLUDE_Shadowsocks_Libev_Server
-config_package_del luci-app-passwall_INCLUDE_Shadowsocks_Rust_Client
-config_package_del luci-app-passwall_INCLUDE_Shadowsocks_Rust_Server
-config_package_del luci-app-passwall_INCLUDE_ShadowsocksR_Libev_Client
-config_package_del luci-app-passwall_INCLUDE_ShadowsocksR_Libev_Server
+config_package_add luci-app-passwall2
+config_package_add luci-app-passwall2_Nftables_Transparent_Proxy
+config_package_del luci-app-passwall2_Iptables_Transparent_Proxy
+config_package_del luci-app-passwall2_INCLUDE_Shadowsocks_Libev_Client
+config_package_del luci-app-passwall2_INCLUDE_Shadowsocks_Libev_Server
+config_package_del luci-app-passwall2_INCLUDE_Shadowsocks_Rust_Client
+config_package_del luci-app-passwall2_INCLUDE_Shadowsocks_Rust_Server
+config_package_del luci-app-passwall2_INCLUDE_ShadowsocksR_Libev_Client
+config_package_del luci-app-passwall2_INCLUDE_ShadowsocksR_Libev_Server
 
 # 镜像生成
 # 修改分区大小
